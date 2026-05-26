d681865e0f32b65fa7f75d7ed2f121d10d507765
ggi737 <ggi73754@gmail.com>
2026-05-26 21:52:11 +0900

    Add files via upload

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from "@capacitor/cli";
+
+const config: CapacitorConfig = {
+  appId: "com.example.haksikalternative",
+  appName: "학식 말고 뭐 먹지?",
+  webDir: "dist/public",
+};
+
+export default config;
